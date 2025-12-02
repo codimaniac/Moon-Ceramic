befdc0e49d959c1b209a36a56b2d603a6685af1c
Codimaniac <simplyanthony007@gmail.com>
2025-12-02 01:16:24 +0100

    Fix: fixed brand history order on mobile

--- src/features/home/components/BrandHistory.tsx
+++ src/features/home/components/BrandHistory.tsx
@@ -6,9 +6,9 @@ const BrandHistory = () => {
     "../src/assets/history-2.png",
   ]
   return (
-    <SectionLayout className="py-8 lg:py-12">
+    <SectionLayout>
         <div className="grid grid-cols-1 min-[820px]:grid-cols-2">
-            <div className="flex flex-col flex-1 items-center justify-center gap-4 px-6 py-12 bg-desert-storm min-[820px]:py-0">
+            <div className="flex flex-col flex-1 order-1 items-center justify-center gap-4 px-6 py-12 bg-desert-storm min-[820px]:py-0">
                 <h2 className="text-neutral-custom-800 text-2xl times text-center font-medium uppercase tracking-wider">
                 Made in Viet Nam since 1450
                 </h2>
@@ -17,9 +17,9 @@ const BrandHistory = () => {
                 Learn More
                 </Button>
             </div>
-            <div className="min-[820px]:flex-1 w-full h-70 bg-cover bg-center" style={{backgroundImage: `url('${ceramicImg[0]}')`}}></div>
-            <div className="min-[820px]:flex-1 w-full h-70 bg-cover bg-center" style={{backgroundImage: `url('${ceramicImg[1]}')`}}></div>
-            <div className="flex flex-col flex-1 items-center justify-center gap-4 px-6 py-12 bg-desert-storm min-[820px]:py-0">
+            <div className="min-[820px]:flex-1 order-2 w-full h-70 bg-cover bg-center" style={{backgroundImage: `url('${ceramicImg[0]}')`}}></div>
+            <div className="min-[820px]:flex-1 order-4 min-[820px]:order-3 w-full h-70 bg-cover bg-center" style={{backgroundImage: `url('${ceramicImg[1]}')`}}></div>
+            <div className="flex flex-col flex-1 order-3 min-[820px]:order-4 items-center justify-center gap-4 px-6 py-12 bg-desert-storm min-[820px]:py-0">
                 <h2 className="text-neutral-custom-800 text-2xl times text-center font-medium uppercase tracking-wider">
                 Our History
                 </h2>
